e4d33f7d98ace6d7089fb2ecc6e63846e0a902b5
lLija <ilija.trokovski@gmail.com>
2023-10-05 16:38:10 +0200

    adding database and finish the app

--- PizzaShop/Pages/Checkout/Checkout.cshtml.cs
+++ PizzaShop/Pages/Checkout/Checkout.cshtml.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PizzaShop.Data;
+using PizzaShop.Models;
 
 namespace PizzaShop.Pages.Checkout
 {
@@ -7,10 +9,13 @@ namespace PizzaShop.Pages.Checkout
     public class CheckoutModel : PageModel
     {  
         public string PizzaName { get; set; }
-
         public float PizzaPrice { get; set; }
-
         public string ImageTitle { get; set; }
+        private readonly ApplicationDbContext _context;
+        public CheckoutModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public void OnGet()
         {
             if (string.IsNullOrEmpty(PizzaName))
@@ -21,6 +26,13 @@ namespace PizzaShop.Pages.Checkout
             {
                 ImageTitle = "Create";
             }
+
+            PizzaOrder pizzaOrder = new PizzaOrder();
+            pizzaOrder.PizzaName = PizzaName;
+            pizzaOrder.BasePrice = PizzaPrice;
+
+            _context.PizzaOrders.Add(pizzaOrder);
+            _context.SaveChanges();
         }
     }
 }
